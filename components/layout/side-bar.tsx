377037bb54ff6acf9ec0a702a38c9e19cdaeaae8
roee-cvt <roeeheily.cvt@gmail.com>
2025-10-19 20:51:37 +0300

    fixed auth problem

--- components/layout/side-bar.tsx
+++ components/layout/side-bar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { startTransition, useState } from 'react'
 import {
   Avatar,
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,11 @@
 
 import { createAuthClient } from 'better-auth/react'
 
-const BASE_URL = process.env.BASE_URL ?? "http://localhost:3000";
+const BASE_URL = process.env.BASE_URL;
+
+if (!BASE_URL) {
+    throw new Error("Missing `BASE_URL` In your app, please fix!")
+}
 
 export const auth =  createAuthClient({
     baseURL: BASE_URL,
