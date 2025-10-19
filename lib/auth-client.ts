6d0c159ed52d38c044e35e5741963547403e5244
roee1454 <roee1454@gmail.com>
2025-10-19 21:09:04 +0300

    fixed base url issue

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,12 +2,12 @@
 
 import { createAuthClient } from 'better-auth/react'
 
-const NEXT_BASE_URL = process.env.NEXT_BASE_URL;
+const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-if (!NEXT_BASE_URL) {
-    throw new Error("Missing `NEXT_BASE_URL` In your app, please fix!")
+if (!NEXT_PUBLIC_BASE_URL) {
+    throw new Error("Missing `NEXT_PUBLIC_BASE_URL` In your app, please fix!")
 }
 
 export const auth =  createAuthClient({
-    baseURL: NEXT_BASE_URL,
+    baseURL: NEXT_PUBLIC_BASE_URL,
 })
